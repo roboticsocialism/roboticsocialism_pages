b69dbb7ab2d0723bc1e57ee50992b91f14f24874
roboticsocialism <roboticsocialism@gmail.com>
2024-08-04 18:34:37 +0800

    Updates

---
+++ precache-manifest.2985792390ce3ac8d0a4073058653135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbb9fd096bb45d9de1b9e48ff94c631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a73e1ee4ccf201a5f32",
+    "url": "/static/css/main.e0eca4b9.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "1a73e1ee4ccf201a5f32",
+    "url": "/static/js/main.ecd34876.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  },
+  {
+    "revision": "04fa3bde593fe1882e364f7afd4e0898",
+    "url": "/static/media/robotics480.04fa3bde.mp4"
+  }
+]);
